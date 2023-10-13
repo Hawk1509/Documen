af4924eb73e845f1d4d156b8766040cd2e77b5c9
Hawk1509 <129725289+Hawk1509@users.noreply.github.com>
2023-10-13 14:29:46 +0530

    pstfix trial

--- joseph/data_structure/postfix.c
+++ joseph/data_structure/postfix.c
@@ -3,7 +3,7 @@
 #include <stdio.h>
 int top = -1;
 int a[10];
-#define max 5;
+#define max 5
 void push();
 void pop();
 void display();
@@ -29,6 +29,48 @@ void pop()
     }
     else
     {
+        item += 1;
+        item = a[top];
         
     }
+}
+char preceed(char);
+char preceed(char ch)
+{
+    switch(ch)
+    {
+        case '(':
+        return 1;
+        case '+':
+        return 2;
+        case '-':
+        return 2;
+        case '*':
+        return 3;
+        case '/':
+        return 3;
+        case '^':
+        return 4;
+        default:
+        return -1; 
+    }
+}
+void main()
+{
+    char item;
+    printf("Enter Expression: ");
+    while(item != '\0')
+    {
+        gets(item);
+    }
+    while(item != '\0')
+    {
+        if (item == '(')
+        {
+            push();
+        }
+        else if (item == alpha)
+    }
+    
+
 }
