3d586adbabfb5484aec28708c5f67d2635a1fbb5
Hawk1509 <129725289+Hawk1509@users.noreply.github.com>
2023-10-16 00:12:14 +0530

    Update trans.java

--- joseph/trans.java
+++ joseph/trans.java
@@ -1,38 +1,40 @@
-//program to display transpose of a matrix
-class trans
-{
-	public static void main(String[] args)
-	{
-		int a[][] = new int[10][10];
-		int b[][] = new int[10][10];
-		int i,j;
-		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter number of rows and columns: ");
-		r = sc.nextInt();
-		c = sc.nextInt();
-		for (i = 0;i<r;i++)
-		{
-			for(j = 0;j<c;j++)
-			{
-				System.out.println("Enter value of matrix A"+i+1 +j+1);
-				a[i][j] = sc.nextInt();
-			}
-		}
-		System.out.println("Transpose is: ");
-		for (j = 0;j<r;j++)
-		{
-			for(i = 0;i<r;i++)
-			{
-				b[j][i] = a[i][j] ;
-			}
-		}
-		
-		for (i = 0;i<c;i++)
-		{
-			for(j = 0;j<r;j++)
-			{
-				System.out.println(+b[i][j]);
-			}
-		}
-	}
+import java.util.Scanner;
+
+class Transpose {
+    public static void main(String[] args) {
+        int r, c, i, j;
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("Enter number of rows: ");
+        r = sc.nextInt();
+        System.out.println("Enter number of columns: ");
+        c = sc.nextInt();
+        
+        int a[][] = new int[r][c];
+        int b[][] = new int[c][r];
+        
+        System.out.println("Enter elements of matrix A: ");
+        for (i = 0; i < r; i++) {
+            for (j = 0; j < c; j++) {
+                System.out.print("Enter value of A[" + (i+1) + "][" + (j+1) + "]: ");
+                a[i][j] = sc.nextInt();
+            }
+        }
+
+        // Transpose the matrix
+        for (j = 0; j < c; j++) {
+            for (i = 0; i < r; i++) {
+                b[j][i] = a[i][j];
+            }
+        }
+
+        // Display the transpose matrix
+        System.out.println("Transpose is: ");
+        for (i = 0; i < c; i++) {
+            for (j = 0; j < r; j++) {
+                System.out.print(b[i][j] + " ");
+            }
+            System.out.println(); // Move to the next row in the transpose matrix
+        }
+    }
 }
