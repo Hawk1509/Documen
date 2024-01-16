0d2773fe086074016b6fcd52f3e455e3ce109456
Hawk1509 <129725289+Hawk1509@users.noreply.github.com>
2024-01-16 11:38:29 +0530

    Update tree.c working

--- joseph/data_structure/tree.c
+++ joseph/data_structure/tree.c
@@ -1,193 +1,304 @@
-//program to create tree
-#include <stdio.h>
-#include <stdlib.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
 struct node
 {
-	int data;
-	struct node *lchild;
-	struct node *rchild;
-};
-struct node *new,*root = NULL,*lchild,*rchild,*ptr,*ptr1,*ptr2;
-int flag = 0,item,a;
-void insert()
+	 int data;
+	 struct node *lchild;
+	 struct node *rchild;
+ }*ptr,*ptr1,*ptr2,*new,*root=NULL,*ptrz,*successorNode,*child,*successorParent;
+ void insert()
+ { 
+ int item;
+ new=(struct node*)malloc (sizeof(struct node));
+ printf("enter the item to be inserted\n");
+ scanf("%d",&item);
+ new->data=item;
+ new->lchild=NULL;
+ new->rchild=NULL;
+if (root==NULL)
 {
-	new = (struct node*)malloc(1*sizeof(struct node));
-	printf("enter element to be inserted: ");
-	scanf("%d",&item);
-	new -> data = item;
-	new -> lchild = NULL;
-	new -> rchild = NULL;
-	ptr = root;
-	while (ptr != NULL)
+    root=new;
+}
+else {	 
+    ptr=root;
+	while(ptr!=NULL)
 	{
-		if (item < ptr -> data) 
-		{
-			ptr1 = ptr;
-			ptr = ptr -> lchild;
-		}
-		else if (item > ptr -> data)
-		{
-			ptr1 = ptr;
-			ptr = ptr -> rchild;
-		}
-		else if (item == ptr -> data)
-		{
-			flag = 1;
-			break;
-		}
+     	ptr1=ptr;
+ 		if (item<ptr->data)
+	 	{	
+		    ptr=ptr->lchild;
+ 		}
+	 	else if (item>ptr->data)
+		{ 			
+	    	ptr=ptr->rchild;
+		}	
 	}
-	if (flag == 1)
-	{
-		printf("Element is present\n");
+	if(ptr1->data>item)
+    {
+		ptr1->lchild=new;			
 	}
 	else
-	{
-		if (root == NULL)
-		{
-			root = new;
-		}
-		else if (ptr1 -> data < item)
-		{
-			ptr1 -> rchild = new;
-		}
-		else 
-		{
-			ptr1 -> lchild = new;
-		}
+    {
+		ptr1->rchild=new;		
+	}	
 	}
+}	
+struct node *successor(struct node * ptr)
+{
+    ptr2 = ptr -> rchild;
+    while (ptr2 -> lchild != NULL && ptr2!= NULL)
+    {
+        ptr2 = ptr2 -> lchild;
+    }
+    return ptr2;
 }
-	
- void inorder( struct node *ptr)
-{
-	if (ptr == NULL)
-	{
-		return;
-	}
-	else
-	{
-		inorder(ptr -> lchild);
-		printf("%d\t",ptr -> data);
-		inorder(ptr -> rchild);
-	}
-} 
-struct node *succ(struct node *ptr)
-{
-	ptr2 = ptr -> rchild;
-	if (ptr2 != NULL)
-	{
-		while(ptr2 -> lchild != NULL)
-		{
-			ptr2 = ptr2 -> lchild;
-		}
-	}
-	return ptr2;
-}
-void del()
-{
-	ptr = root;
-	int flag = 0;
-	printf("enter element to be deleted: ");
-	scanf("%d",&item);
-	while (ptr != NULL && flag == 0)
-	{
-		if (item < ptr -> data) 
-		{
-			ptr1 = ptr;
-			ptr = ptr -> lchild;
-		}
-		else if (item > ptr -> data)
-		{
-			ptr1 = ptr;
-			ptr = ptr -> rchild;
-		}
-		else if (item == ptr -> data)
-		{
-			flag = 1;
-			break;
-		}
-	}
-	if (flag == 0)
-	{
-		printf("Element is not present.\n");
-	}
-	else 
-	{
-		if(ptr -> lchild == NULL && ptr -> rchild == NULL)
-		{
-			if (ptr1 -> lchild == ptr)
-			{
-				ptr1 -> lchild = NULL;
-			}
-			else
-			{
-				ptr1 -> rchild = NULL;
-			}
-			free(ptr);
-		}
-		else if(ptr -> lchild != NULL && ptr -> rchild == NULL || ptr -> lchild == NULL && ptr -> rchild != NULL) 
-		{
-			if(ptr1 -> lchild == ptr)
-			{
-				if(ptr -> lchild != NULL)
-				{
-					ptr1 -> lchild = ptr -> lchild;
-				}
-				else
-				{
-					ptr1 -> rchild = ptr -> rchild;
-				}
-				free(ptr);
-			}
-			else if(ptr1 -> rchild == ptr)
-			{
-				if(ptr -> rchild != NULL)
-				{
-					ptr1 -> lchild = ptr -> lchild;
-				}
-				else
-				{
-					ptr1 -> rchild = ptr -> rchild;
-				}
-				free(ptr);
-			}
-			
 
-		}
-		
-		else if (ptr -> rchild != NULL && ptr -> lchild != NULL)
-		{
-			ptr2 = succ(ptr);
-			a = ptr2 -> data;
-			ptr -> data = a;
-			del(a);
-			free(ptr);
-		}
-	}
-}
-void main()
+void display(struct node*ptr)
 {
-	int ch;
-	char c;
-	do
-	{
-		printf("\n1.Insertion\n2.Deletion\n3.Display\nEnter choice: ");
-		scanf("%d", &ch);
-		switch(ch)
-		{
-			case 1: insert();
-				break;
-			case 2: del();
-				break;
-			case 3: inorder(root);
-				break;
-		}
-		printf("\nContinue(y/n)? ");
-		scanf(" %c",&c);
-	}
-	while (c == 'y');
+   	if(ptr==NULL)
+   	return;
+   	display(ptr->lchild);
+   	printf("%d\t",ptr->data);
+   	display(ptr->rchild);  	
 }
-			
-		
-	
-	
-	
+void deleteNode(int item) {
+    int f = 0;
+    if (root == NULL) {
+        printf("Tree is empty\n");
+    } else {
+        ptr = root;
+        ptr1 = NULL;
+        while (ptr != NULL && f == 0) {
+            if (item < ptr->data) {
+                ptr1 = ptr;
+                ptr = ptr->lchild;
+            } else if (item > ptr->data) {
+                ptr1 = ptr;
+                ptr = ptr->rchild;
+            } else if (item == ptr->data) {
+                f = 1;
+                break;
+            }
+        }
+        if (f == 0) {
+            printf("Element not found\n");
+        } else {
+            if (ptr->lchild == NULL && ptr->rchild == NULL) { // case 1
+                if (ptr1 == NULL) {
+                    // Deleting the root when it has no children
+                    root = NULL;
+                } else if (ptr == ptr1->lchild) {
+                    ptr1->lchild = NULL;
+                } else {
+                    ptr1->rchild = NULL;
+                }
+
+                printf("The element deleted is %d\n", ptr->data);
+                free(ptr);
+            } else if ((ptr->lchild != NULL && ptr->rchild == NULL) || (ptr->rchild != NULL && ptr->lchild == NULL)) {
+                // case 2
+                child = (ptr->lchild != NULL) ? ptr->lchild : ptr->rchild;
+
+                if (ptr1 == NULL) {
+                    // Deleting the root with one child
+                    root = child;
+                } else if (ptr == ptr1->lchild) {
+                    ptr1->lchild = child;
+                } else {
+                    ptr1->rchild = child;
+                }
+
+                printf("The element deleted is %d\n", ptr->data);
+                free(ptr);
+            } else if (ptr->lchild != NULL && ptr->rchild != NULL) { // case 3
+                successorParent = ptr;
+                successorNode = ptr->rchild;
+
+                while (successorNode->lchild != NULL) {
+                    successorParent = successorNode;
+                    successorNode = successorNode->lchild;
+                }
+                ptr->data = successorNode->data;
+                // Now, delete the successor node
+                if (successorParent == ptr) {
+                    successorParent->rchild = successorNode->rchild;
+                } else {
+                    successorParent->lchild = successorNode->rchild;
+                }
+
+                free(successorNode);
+            }
+        }
+    }
+}
+ void main()
+ {
+  int c=0;
+ 	do{
+  		printf("\nenter the choise\n(1)-insert\n(2)-display\n(3)delete\n(0)-exit\n");
+  		scanf("%d",&c);
+  		switch(c)
+  		{
+  			 case 1:
+  			 insert();
+  			 break;
+  			 
+  			 case 2:
+  			 display(root);
+  			 break;
+  			 
+  			 case 3:
+             int item;
+                printf("Enter the element to delete: ");
+                scanf("%d", &item);
+  			 deleteNode(item);
+  			 break;
+  		
+  		
+  		}
+  	
+  	}while(c!=0);
+ }
+ /*
+ OUTPUT
+ enter the choise
+(1)-insert
+(2)-display
+(3)delete
+(0)-exit
+1
+enter the item to be inserted
+9
+
+enter the choise
+(1)-insert
+(2)-display
+(3)delete
+(0)-exit
+1
+enter the item to be inserted
+11
+
+enter the choise
+(1)-insert
+(2)-display
+(3)delete
+(0)-exit
+1
+enter the item to be inserted
+7
+
+enter the choise
+(1)-insert
+(2)-display
+(3)delete
+(0)-exit
+1
+enter the item to be inserted
+4
+
+enter the choise
+(1)-insert
+(2)-display
+(3)delete
+(0)-exit
+1
+enter the item to be inserted
+1
+
+enter the choise
+(1)-insert
+(2)-display
+(3)delete
+(0)-exit
+1
+enter the item to be inserted
+5
+
+enter the choise
+(1)-insert
+(2)-display
+(3)delete
+(0)-exit
+2
+1       4       5       7       9       11
+enter the choise
+(1)-insert
+(2)-display
+(3)delete
+(0)-exit
+3
+Enter the element to delete: 1
+The element deleted is 1
+
+enter the choise
+(1)-insert
+(2)-display
+(3)delete
+(0)-exit
+2
+4       5       7       9       11
+enter the choise
+(1)-insert
+(2)-display
+(3)delete
+(0)-exit
+3
+Enter the element to delete: 5
+The element deleted is 5
+
+enter the choise
+(1)-insert
+(2)-display
+(3)delete
+(0)-exit
+2
+4       7       9       11
+enter the choise
+(1)-insert
+(2)-display
+(3)delete
+(0)-exit
+1
+enter the item to be inserted
+1
+
+enter the choise
+(1)-insert
+(2)-display
+(3)delete
+(0)-exit
+1
+enter the item to be inserted
+5
+
+enter the choise
+(1)-insert
+(2)-display
+(3)delete
+(0)-exit
+2
+1       4       5       7       9       11
+enter the choise
+(1)-insert
+(2)-display
+(3)delete
+(0)-exit
+3
+Enter the element to delete: 4
+
+enter the choise
+(1)-insert
+(2)-display
+(3)delete
+(0)-exit
+2
+1       5       7       9       11
+enter the choise
+(1)-insert
+(2)-display
+(3)delete
+(0)-exit
+0
+*/
